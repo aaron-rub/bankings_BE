4c2855d0e16e462e294555f702eff21379c99515
Haoxuan Tong <haoxuantong8@gmail.com>
2024-02-13 23:28:40 -0800

    transfer

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -122,4 +122,26 @@ public class PersonApiController {
         // return Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
     }
+
+    @PostMapping(value = "/transfer/{id1}/{id2}/{day}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> personSearch(@RequestBody final Map<String,String> map) {
+        // locate the users
+        Optional<Person> optional1 = repository.findById(id1);
+        Optional<Person> optional2 = repository.findById(id2);
+        if (optional1.isPresent() && optional2.isPresent()) {  // Good ID
+            Person user1 = optional1.get();  // value from findByID
+            Person user2 = optional2.get();
+            for (Map.Entry<String,Object> entry : user.stat_map.entrySet())  {
+                if (entry.getKey().equals("date")) {
+                    Map<String, Object> transfered = new HashMap<>();
+                    transfered.put(entry.getKey(), entry.getValue());
+                    user2.stat_map.putAll(transfered);
+                    user1.stat_map.remove(entry.getKey());
+                }
+            }
+            return new ResponseEntity<>(transfered, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+    }
 }
